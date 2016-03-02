5b733b78a7b45d437b31394eb910d10b3f9e1e44
oakes <zsoakes@gmail.com>
2016-03-02 10:18:41 -0500

    Add exercise 8

---
+++ src/Exercise08.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/**
+ * Created by zach on 3/2/16.
+ */
+public class Exercise08 {
+    public static void main(String[] args) {
+        String[] names = {"Charlie", "David", "Alice", "Bob"};
+        ArrayList<String> sortedNames = new ArrayList<>();
+        for (String name : names) {
+            int index = 0;
+            for (String sortedName : sortedNames) {
+                if (name.compareTo(sortedName) > 0) {
+                    index++;
+                }
+                else {
+                    break;
+                }
+            }
+            sortedNames.add(index, name);
+        }
+        System.out.println(sortedNames);
+    }
+}
